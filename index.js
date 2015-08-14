68855756a3ecff3f1f90ca96361250c48221f1ab
Pawel Galazka <pawel.galazka1986@gmail.com>
2015-08-14 21:47:47 +0200

    run api: better command call

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var chalk = require('chalk');
 var execSync = require('child_process').execSync;
 
-module.exports = function(){
-    console.log(chalk.bold(arguments[0]));
-    var p = execSync.apply(null, arguments);
+module.exports = function(cmd){
+    console.log(chalk.bold(cmd));
+    var p = execSync.call(null, cmd);
     console.log(p.toString());
 };
