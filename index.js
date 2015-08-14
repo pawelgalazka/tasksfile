3a3251592ff299ee179000edea47d4c7c379c38b
Pawel Galazka <pawel.galazka1986@gmail.com>
2015-08-14 22:10:45 +0200

    Taking current node module bin dir into path for run api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var execSync = require('child_process').execSync;
 
 module.exports = function(cmd){
     console.log(chalk.bold(cmd));
+    cmd = 'PATH=$PATH:./node_modules/.bin/ ' + cmd;
     var p = execSync.call(null, cmd);
     console.log(p.toString());
 };
