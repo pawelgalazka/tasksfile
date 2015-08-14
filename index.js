fd043a97a6dc0a8cc1bde5c56540e09a735cdef8
Pawel Galazka <pawel.galazka1986@gmail.com>
2015-08-14 23:10:35 +0200

    Refactoring

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var chalk = require('chalk');
 var execSync = require('child_process').execSync;
 
-module.exports = function(cmd){
+exports.call = function(obj, args){
+    var taskName = args[1];
+    if(!taskName){
+        console.log('Available tasks:');
+        Object.keys(obj).forEach(function(t){
+            console.log(t);
+        });
+        return;
+    }
+
+    var task = obj[taskName];
+    if(task){
+        obj[taskName].apply(null, args.slice(2));
+    }
+    else {
+        console.error(chalk.red("Task " + taskName + " not found"));
+    }
+};
+
+exports.run = function(cmd){
     console.log(chalk.bold(cmd));
     cmd = 'PATH=$PATH:./node_modules/.bin/ ' + cmd;
     execSync(cmd, {stdio: 'inherit'});
--- runfile.js
+++ runfile.js
@@ -1,5 +1,9 @@
-var run = require('runjs');
+var run = require('runjs').run;
 
 exports.tests = function(){
     run('jasmine');
+};
+
+exports.echo = function(){
+    console.log('echo ' + Array.prototype.slice.call(arguments, 0).join(' '))
 };
