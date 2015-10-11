e63e53e70158abde0fba0907bf464714cc4797c5
Pawel Galazka <pawel.galazka1986@gmail.com>
2015-10-11 10:28:28 +0200

    More pleasant error handling

--- bin/run.js
+++ bin/run.js
@@ -2,6 +2,9 @@
 
 var call = require('../lib/index').call;
 require("babel/register");
-var runfile = require(process.cwd() + '/runfile');
-
-call(runfile, process.argv.slice(2));
+try {
+    var runfile = require(process.cwd() + '/runfile');
+    call(runfile, process.argv.slice(2));
+} catch (e){
+    console.error(e.name, ':' ,e.message);
+}
