f0ce78f23f1e0ac3ce3f390b41532b63156664a7
zephraph <zephraph@gmail.com>
2015-12-14 14:58:02 -0500

    Add robust babel support

--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 
-var call = require('../lib/index').call;
-require("babel/register");
+var config = require('pkg-config')('runjs');
+var call   = require('../lib/index').call;
+
+if (config && config['require-hook']) {
+  require(config['require-hook']);
+}
+else {
+
+    try {
+        require('babel-register');
+    } catch() {
+        try {
+            require('babel/register');
+        } catch() {}
+    }
+}
+
 try {
     var runfile = require(process.cwd() + '/runfile');
     call(runfile, process.argv.slice(2));
