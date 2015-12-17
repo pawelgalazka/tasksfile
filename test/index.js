d9f38127a4152d02c60094798d3414c9639e0b66
Pawel Galazka <pawel.galazka1986@gmail.com>
2015-12-17 18:51:03 +0100

    Tests: introducing more es6 syntax

--- test/index.js
+++ test/index.js
@@ -3,42 +3,43 @@ import chalk from 'chalk';
 import {expect, spy} from 'mochaccino';
 
 
-describe('index.js', function(){
-    describe('.call()', function(){
-        beforeEach(function(){
-            this.a = spy();
-            this.b = spy();
-            this.obj = {
-                a: this.a,
-                b: this.b
+describe('index.js', () => {
+    describe('.call()', () => {
+        let a, b, obj, consl;
+        beforeEach(() => {
+            a = spy();
+            b = spy();
+            obj = {
+                a: a,
+                b: b
             };
-            this.console = {
+            consl = {
                 log: spy()
             };
         });
 
-        it('should call method with given name on given object', function(){
-            runjs.call(this.obj, ['a'], this.console);
-            expect(this.a).toHaveBeenCalled();
+        it('should call method with given name on given object', () => {
+            runjs.call(obj, ['a'], consl);
+            expect(a).toHaveBeenCalled();
         });
 
-        it('should call method with given name on given object with given arguments', function(){
-            runjs.call(this.obj, ['b', '1', '2'], this.console);
-            expect(this.b).toHaveBeenCalledWith('1', '2');
+        it('should call method with given name on given object with given arguments', () => {
+            runjs.call(obj, ['b', '1', '2'], consl);
+            expect(b).toHaveBeenCalledWith('1', '2');
         });
 
-        it('should print list of all methods available in object if method name not given', function(){
-            runjs.call(this.obj, [], this.console);
-            expect(this.console.log).toHaveBeenCalledTimes(3);
-            expect(this.console.log).toHaveBeenCalledWith('Available tasks:');
-            expect(this.console.log).toHaveBeenCalledWith('a');
-            expect(this.console.log).toHaveBeenCalledWith('b');
+        it('should print list of all methods available in object if method name not given', () => {
+            runjs.call(obj, [], consl);
+            expect(consl.log).toHaveBeenCalledTimes(3);
+            expect(consl.log).toHaveBeenCalledWith('Available tasks:');
+            expect(consl.log).toHaveBeenCalledWith('a');
+            expect(consl.log).toHaveBeenCalledWith('b');
         });
 
-        it('should print error message if method not exist on given object', function(){
-            runjs.call(this.obj, ['abc'], this.console);
-            expect(this.console.log).toHaveBeenCalledTimes(1);
-            expect(this.console.log).toHaveBeenCalledWith(chalk.red('Task abc not found'));
+        it('should print error message if method not exist on given object', () => {
+            runjs.call(obj, ['abc'], consl);
+            expect(consl.log).toHaveBeenCalledTimes(1);
+            expect(consl.log).toHaveBeenCalledWith(chalk.red('Task abc not found'));
         });
     });
 });
