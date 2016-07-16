b0a80e71ef579e0cbb844a9c693e7d46dd18f775
Pawel Galazka <pawel.galazka1986@gmail.com>
2016-07-16 20:22:40 +1000

    Run script: progressing messages for babel-register load

--- bin/run.js
+++ bin/run.js
@@ -2,7 +2,9 @@
 
 var call = require('../lib/index').call;
 
+console.log('Requiring babel-register...')
 require(process.cwd() + '/node_modules/babel-register');
 
+console.log('Processing runfile...')
 var runfile = require(process.cwd() + '/runfile');
 call(runfile, process.argv.slice(2));
