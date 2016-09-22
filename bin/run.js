6f9a8f8dc67ef4d27ac221baa49e63b24a3962e8
Pawel Galazka <pawel.galazka1986@gmail.com>
2016-09-22 18:31:40 +1000

    run script: using path.resolve

--- bin/run.js
+++ bin/run.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 
 var call = require('../lib/index').call;
+var path = require('path');
+
+// try to read package.json config
 try {
-  var pkg = require(process.cwd() + '/package.json') || {};
+  var pkg = require(path.resolve('./package.json')) || {};
 } catch (e) {
 
 }
 
-
+// try to load babel-register
 try {
   console.log('Requiring babel-register...')
   if (pkg.runjs && pkg.runjs['babel-register']) {
-    require(process.cwd() + pkg.runjs['babel-register']);
+    require(path.resolve(pkg.runjs['babel-register']));
   } else {
-    require(process.cwd() + '/node_modules/babel-register');
+    require(path.resolve('./node_modules/babel-register'));
   }
 } catch (e) {
   console.log('Requiring failed. Fallback to pure node.')
 }
 
+// process runfile.js
 console.log('Processing runfile...')
-var runfile = require(process.cwd() + '/runfile');
+var runfile = require(path.resolve('./runfile'));
 call(runfile, process.argv.slice(2));
