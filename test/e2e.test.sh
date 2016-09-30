3cbcb09e310ed0e835ca1bcf0ee6e04b69cfd291
Pawel Galazka <pawel.galazka1986@gmail.com>
2016-09-30 15:42:59 +1000

    Moving e2e tests to bash scripting

---
+++ test/e2e.test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Error messages are redirected to stderr
+function handle_error {
+  echo "$(basename $0): ERROR! An error was encountered executing line $1." 1>&2;
+  echo 'Exiting with error.' 1>&2;
+  exit 1
+}
+
+# Exit the script with a helpful error message when any error is encountered
+trap 'set +x; handle_error $LINENO $BASH_COMMAND' ERR
+
+# Echo every command being executed
+set -x
+
+echo 'Sandbox tests'
+cd test/sandbox
+
+echo 'Cleaning node_modules'
+rm -rf node_modules
+
+echo 'Installing runjs'
+npm install ../../
+
+echo 'Installing test package'
+npm install
+
+echo 'Testing test package'
+npm test
