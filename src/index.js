7b288d4e4eebee3fdf227827176ca36220fed68a
Pawel Galazka <pawel.galazka1986@gmail.com>
2016-12-07 18:25:56 +1100

    Introducing linting into development process

--- src/index.js
+++ src/index.js
@@ -1,56 +1,54 @@
-import chalk from 'chalk';
-import chProcess from 'child_process';
-import template from 'lodash.template';
-import fs from 'fs';
-import path from 'path';
+import chalk from 'chalk'
+import chProcess from 'child_process'
+import template from 'lodash.template'
+import fs from 'fs'
+import path from 'path'
 
-export function call(obj, args, cons = console) {
-  let taskName = args[0];
+export function call (obj, args, cons = console) {
+  let taskName = args[0]
 
   if (obj.default) {
-    obj = obj.default;
+    obj = obj.default
   }
 
   if (!taskName) {
-    cons.log('Available tasks:');
+    cons.log('Available tasks:')
     Object.keys(obj).forEach((t) => {
-      cons.log(t);
-    });
-    return;
+      cons.log(t)
+    })
+    return
   }
 
   Object.keys(obj).forEach((t) => {
-    let task = obj[t];
+    let task = obj[t]
     obj[t] = function () {
-      let time = Date.now();
-      cons.log(chalk.blue(`Running "${t}"...`));
-      task.apply(null, arguments);
-      time = ((Date.now() - time) / 1000).toFixed(2);
-      cons.log(chalk.blue(`Finished "${t}" in ${time} sec`));
+      let time = Date.now()
+      cons.log(chalk.blue(`Running "${t}"...`))
+      task.apply(null, arguments)
+      time = ((Date.now() - time) / 1000).toFixed(2)
+      cons.log(chalk.blue(`Finished "${t}" in ${time} sec`))
     }
-  });
+  })
 
-  let task = obj[taskName];
+  let task = obj[taskName]
   if (task) {
-    obj[taskName].apply(null, args.slice(1));
-  }
-  else {
-    cons.log(chalk.red(`Task ${taskName} not found`));
+    obj[taskName].apply(null, args.slice(1))
+  } else {
+    cons.log(chalk.red(`Task ${taskName} not found`))
   }
 }
 
-export function run(cmd, options = {}) {
-  options.env = options.env || process.env;
-  let envPath = options.env.PATH ? options.env.PATH : process.env.PATH;
+export function run (cmd, options = {}) {
+  options.env = options.env || process.env
+  let envPath = options.env.PATH ? options.env.PATH : process.env.PATH
 
   options.env.PATH = [
     `${process.cwd()}/node_modules/.bin/`, envPath
-  ].join(path.delimiter);
+  ].join(path.delimiter)
 
-  options.stdio = options.stdio || 'inherit';
-  console.log(chalk.bold(cmd));
+  options.stdio = options.stdio || 'inherit'
+  console.log(chalk.bold(cmd))
   if (options.async) {
-
     return new Promise((resolve, reject) => {
       const asyncProcess = chProcess.exec(cmd, options, (error, stdout, stderr) => {
         if (error) {
@@ -58,7 +56,7 @@ export function run(cmd, options = {}) {
         } else {
           resolve(stdout)
         }
-      });
+      })
 
       if (options.stdio === 'inherit') {
         asyncProcess.stdout.pipe(process.stdout)
@@ -66,12 +64,12 @@ export function run(cmd, options = {}) {
     })
   }
 
-  return chProcess.execSync(cmd, options);
+  return chProcess.execSync(cmd, options)
 }
 
-export function generate(src, dst, context) {
-  console.log(`Generating ${dst} from template ${src}`);
-  let templateString = fs.readFileSync(src);
-  let content = template(templateString)(context);
-  fs.writeFileSync(dst, content);
+export function generate (src, dst, context) {
+  console.log(`Generating ${dst} from template ${src}`)
+  let templateString = fs.readFileSync(src)
+  let content = template(templateString)(context)
+  fs.writeFileSync(dst, content)
 }
