86ff93e98663766a854d8f59369cc5c3bf3d42ba
Pawel Galazka <pawel.galazka1986@gmail.com>
2016-12-07 20:36:54 +1100

    Making load function testable

--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
-
+const path = require('path')
+const fs = require('fs')
 const api = require('../lib/index')
-const runfile = api.load('./runfile')
+
+const runfile = api.load('./runfile', console, function (filePath){
+  return require(path.resolve(filePath))
+}, function (filePath){
+  fs.accessSync(path.resolve(filePath))
+}, function (code = 0){
+  process.exit(code)
+})
+
 api.call(runfile, process.argv.slice(2))
--- src/index.js
+++ src/index.js
@@ -4,42 +4,42 @@ import template from 'lodash.template'
 import fs from 'fs'
 import path from 'path'
 
-export function load (runfilePath) {
+export function load (runfilePath, logger, requirer, access, exit) {
   let config
 
   // try to read package.json config
   try {
-    config = require(path.resolve('./package.json')).runjs || {}
+    config = requirer('./package.json').runjs || {}
   } catch (error) {
     config = {}
   }
 
   // try to load babel-register
   try {
-    console.log('Requiring babel-register...')
+    logger.log('Requiring babel-register...')
     if (config['babel-register']) {
-      require(path.resolve(config['babel-register']))
+      requirer(config['babel-register'])
     } else {
-      require(path.resolve('./node_modules/babel-register'))
+      requirer('./node_modules/babel-register')
     }
   } catch (error) {
-    console.log('Requiring failed. Fallback to pure node.')
+    logger.log('Requiring failed. Fallback to pure node.')
     if (config['babel-register']) {
       throw error.stack
     }
   }
 
   // process runfile.js
-  console.log('Processing runfile...')
+  logger.log('Processing runfile...')
 
   try {
-    fs.accessSync(path.resolve('./runfile.js'))
+    access('./runfile.js')
   } catch (error) {
-    console.log(`No runfile.js defined in ${process.cwd()}`)
-    process.exit(1)
+    logger.log(`No runfile.js defined in ${process.cwd()}`)
+    exit(1)
   }
 
-  const runfile = require(path.resolve('./runfile'))
+  const runfile = requirer('./runfile')
   return runfile
 }
 
