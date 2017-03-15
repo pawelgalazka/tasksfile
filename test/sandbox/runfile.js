0256f0d9faba5bd7cc1c32216e1da4abf702de6a
Pawel Galazka <pawel.galazka1986@gmail.com>
2017-03-15 22:07:28 +1100

    Changing sandbox runfile.js format

--- test/sandbox/runfile.js
+++ test/sandbox/runfile.js
@@ -1,47 +1,51 @@
 import { run, ask } from 'runjs'
 
-const task = {
-  'echo': (...args) => {
-    console.log('echo ' + args.join(' '))
-  },
-  'testapi': () => {
-    run('ls -al')
-    run('echo "\\033[33;44m Yellow text on blue background\\033[0m"')
-  },
-  'testerror': (async) => {
-    if (async) {
-      run('node ./error_script.js', {async: true}).catch((error) => {
-        console.log('ERROR', error)
-      })
-    } else {
-      run('node ./error_script.js')
-    }
-  },
-  'testserver': () => {
-    run('http-server', {async: true})
-  },
-  'testasync': () => {
-    run('ls -al', {async: true}).then((data) => {
-      console.log('DATA', data)
+export function echo (...args) {
+  console.log('echo ' + args.join(' '))
+}
+
+export function testapi () {
+  run('ls -al')
+  run('echo "\\033[33;44m Yellow text on blue background\\033[0m"')
+}
+
+export function testerror (async) {
+  if (async) {
+    run('node ./error_script.js', {async: true}).catch((error) => {
+      console.log('ERROR', error)
     })
-  },
-  'n1': {
-    'nested1': () => {
-      console.log('Nested task nr 1 executed!')
-    }
-  },
-  'n1:nested2': () => {
-    console.log('Nested task nr 2 executed!')
-  },
-  'testask': () => {
-    ask('Who are you?').then((answer) => {
-      console.log(`Hello ${answer}!`)
-    })
+  } else {
+    run('node ./error_script.js')
   }
-};
+}
 
-task['echo'].doc = 'Simple echo task'
-task['n1']['nested1'].doc = 'Description of nested task nr 1'
-task['n1:nested2'].doc = 'Description of nested task nr 2'
+export function testserver () {
+  run('http-server', {async: true})
+}
 
-export default task
+export function testasync () {
+  run('ls -al', {async: true}).then((data) => {
+    console.log('DATA', data)
+  })
+}
+
+export function testask () {
+  ask('Who are you?').then((answer) => {
+    console.log(`Hello ${answer}!`)
+  })
+}
+
+export const n1 = {
+  nested1 () {
+    console.log('Nested task nr 1 executed!')
+  },
+
+  'nested2:echo': () => {
+    console.log('Nested task nr 2 executed!')
+  }
+}
+
+
+echo.doc = 'Simple echo task'
+n1.nested1.doc = 'Description of nested task nr 1'
+n1['nested2:echo'].doc = 'Description of nested task nr 2'
