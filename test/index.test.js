c3cb8ca862abc53c3e908b5f6e58b31bbe683aaa
Pawel Galazka <pawel.galazka1986@gmail.com>
2017-03-21 14:09:08 +1100

    Index tests: group by async and sync

--- test/index.test.js
+++ test/index.test.js
@@ -16,24 +16,28 @@ describe('api', () => {
   })
 
   describe('run()', () => {
-    it('should execute basic shell commands when sync mode', () => {
-      const output = api.run('echo "echo test"', {cwd: './test/sandbox'}, logger)
-      expect(output).toEqual('echo test\n')
-      expect(logger.info).toHaveBeenCalledWith('echo "echo test"')
-    })
-
-    it('should execute basic shell commands when async mode', (done) => {
-      api.run('echo "echo test"', {async: true}, logger).then((output) => {
+    describe('sync version', () => {
+      it('should execute basic shell commands', () => {
+        const output = api.run('echo "echo test"', {cwd: './test/sandbox'}, logger)
         expect(output).toEqual('echo test\n')
         expect(logger.info).toHaveBeenCalledWith('echo "echo test"')
-        done()
+      })
+
+      it('should throw an error if command fails', () => {
+        expect(() => {
+          api.run('node ./ghost.js', {stdio: 'pipe'}, logger)
+        }).toThrow(RunJSError)
       })
     })
 
-    it('should throw an error if command fails', () => {
-      expect(() => {
-        api.run('node ./ghost.js', {stdio: 'pipe'}, logger)
-      }).toThrow(RunJSError)
+    describe('async version', () => {
+      it('should execute basic shell commands', (done) => {
+        api.run('echo "echo test"', {async: true}, logger).then((output) => {
+          expect(output).toEqual('echo test\n')
+          expect(logger.info).toHaveBeenCalledWith('echo "echo test"')
+          done()
+        })
+      })
     })
   })
 
