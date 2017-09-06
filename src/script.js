fa35098649c9d67695605183dd1f85a81f5c9c44
Paweł Gałązka <pawel.galazka@envato.com>
2017-09-06 22:49:14 +1000

    Styling Available tasks title

--- src/script.js
+++ src/script.js
@@ -83,7 +83,7 @@ function parseArgs (args: Array<string>) {
 
 function describe (obj: Object, logger: Logger, namespace: ?string) {
   if (!namespace) {
-    logger.log(chalk.dim('Available tasks:\n'))
+    logger.log(chalk.yellow('\nAvailable tasks:'))
   }
 
   Object.keys(obj).forEach((key) => {
