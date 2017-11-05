0e776aa6683dda650c4b1aff99b31e6ef8932bec
Paweł Gałązka <pawel.galazka@envato.com>
2017-11-05 16:02:53 +1100

    Reformatting by prettier

--- test/e2e/sandbox/babel/runfile.js
+++ test/e2e/sandbox/babel/runfile.js
@@ -1,41 +1,45 @@
 import { run, options } from '../../../../lib'
 
-export function echo (...args) {
+export function echo(...args) {
   console.log('echo', args, options(this))
 }
 
-export function commands () {
+export function commands() {
   run('echo "sync terminal"')
   console.log('output', run('echo "sync pipe"', { stdio: 'pipe' }))
   run('echo "async terminal"', { async: true })
-  run('echo "async terminal"', { async: true, stdio: 'pipe' })
-    .then(output => console.log('output', output))
+  run('echo "async terminal"', { async: true, stdio: 'pipe' }).then(output =>
+    console.log('output', output)
+  )
 }
 
 export const nested = {
-  echo (...args) {
+  echo(...args) {
     console.log('nested echo', args, options(this))
   }
 }
 
-export function localbin () {
+export function localbin() {
   run('hello')
 }
 
-export async function asyncawait () {
-  const output = await run('echo "async and await"', {async: true, stdio: 'pipe'})
+export async function asyncawait() {
+  const output = await run('echo "async and await"', {
+    async: true,
+    stdio: 'pipe'
+  })
   console.log('output', output)
   console.log('after await')
 }
 
-export function error () {
-  run('node ../../scripts/error.js', {async: true})
+export function error() {
+  run('node ../../scripts/error.js', { async: true })
   run('node ../../scripts/error.js')
 }
 
-export function color () {
+export function color() {
   run('node ../../scripts/color.js')
-  run('node ../../scripts/color.js', {async: true})
+  run('node ../../scripts/color.js', { async: true })
 }
 
 echo.help = 'Simple echo task'
