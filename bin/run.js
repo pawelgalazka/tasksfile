9b3570c58fefdb5cfcd5cbc66e3e7b2e8a527b57
s weingarten <sweingarten@users.noreply.github.com>
2018-05-22 17:52:08 -0700

    path.resolve -> require.resolve
    this small change restores functionality when runjs installed in a Lerna monorepo (or any repo where the node_modules folder isn't in the standard location)

--- bin/run.js
+++ bin/run.js
@@ -2,10 +2,10 @@
 const path = require('path')
 const fs = require('fs')
 
-const SCRIPT_API_PATH = './node_modules/runjs/lib/script.js'
+const SCRIPT_API_PATH = 'runjs/lib/script.js'
 
 try {
-  fs.accessSync(path.resolve(SCRIPT_API_PATH))
+  fs.accessSync(require.resolve(SCRIPT_API_PATH))
 } catch (error) {
   console.error(
     'RunJS not found. Do "npm install runjs --save-dev" or "yarn add --dev runjs" first.'
@@ -13,7 +13,7 @@ try {
   process.exit(1)
 }
 
-const script = require(path.resolve(SCRIPT_API_PATH))
+const script = require(require.resolve(SCRIPT_API_PATH))
 
 if (!script.main) {
   console.error(
