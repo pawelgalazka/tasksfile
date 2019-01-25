e4c38e35d7fad7fd7361f2f7701f8c0232fc348a
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-01-25 22:09:38 +1100

    Add to shell util sync version

--- src/shell.ts
+++ src/shell.ts
@@ -1,28 +1,41 @@
-import { spawn, StdioOptions } from 'child_process'
+import { execSync, spawn, StdioOptions } from 'child_process'
 
-interface IShellOptions {
+export class ShellError extends Error {
+  constructor(message: string) {
+    message = message && message.split('\n')[0] // assign only first line
+    super(message)
+  }
+}
+
+interface ICommonShellOptions {
   cwd?: string
   env?: NodeJS.ProcessEnv
-  stdio: StdioOptions
+  stdio?: StdioOptions
   timeout?: number
 }
 
-export default (
+export interface IShellOptions extends ICommonShellOptions {
+  async?: boolean
+}
+
+function shellAsync(
   command: string,
-  { cwd, env, timeout, stdio = 'inherit' }: IShellOptions
-): Promise<string | null> => {
+  options: ICommonShellOptions = {}
+): Promise<string | null> {
   return new Promise((resolve, reject) => {
-    const asyncProcess = spawn(command, {
-      cwd,
-      env,
+    const nextOptions = {
+      ...options,
       shell: true,
-      stdio
-    })
+      stdio: options.stdio || 'inherit'
+    }
+    const asyncProcess = spawn(command, nextOptions)
     let output: string | null = null
 
     asyncProcess.on('error', (error: Error) => {
       reject(
-        new Error(`Failed to start command: ${command}; ${error.toString()}`)
+        new ShellError(
+          `Failed to start command: ${command}; ${error.toString()}`
+        )
       )
     })
 
@@ -31,22 +44,61 @@ export default (
         resolve(output)
       } else {
         reject(
-          new Error(`Command failed: ${command} with exit code ${exitCode}`)
+          new ShellError(
+            `Command failed: ${command} with exit code ${exitCode}`
+          )
         )
       }
     })
 
-    if (stdio === 'pipe') {
+    if (nextOptions.stdio === 'pipe') {
       asyncProcess.stdout.on('data', (buffer: Buffer) => {
         output = buffer.toString()
       })
     }
 
-    if (timeout) {
+    if (nextOptions.timeout) {
       setTimeout(() => {
         asyncProcess.kill()
-        reject(new Error(`Command timeout: ${command}`))
-      }, timeout)
+        reject(new ShellError(`Command timeout: ${command}`))
+      }, nextOptions.timeout)
     }
   })
 }
+
+function shellSync(
+  command: string,
+  options: ICommonShellOptions = {}
+): string | null {
+  try {
+    const nextOptions = {
+      ...options,
+      stdio: options.stdio || 'inherit'
+    }
+    const buffer: string | Buffer = execSync(command, nextOptions)
+    if (buffer) {
+      return buffer.toString()
+    }
+    return null
+  } catch (error) {
+    throw new ShellError(error.message)
+  }
+}
+
+function shell(
+  command: string,
+  options: IShellOptions & { async: true }
+): Promise<string | null>
+
+function shell(
+  command: string,
+  options?: IShellOptions & { async?: false }
+): string | null
+
+function shell(command: string, options?: IShellOptions) {
+  return options && options.async
+    ? shellAsync(command, options)
+    : shellSync(command, options)
+}
+
+export default shell
