cf2b6a69faa5660fe4f499d7a9b7eb82f5f629ed
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-19 08:54:37 +1000

    sh tests: remove empty context

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -14,7 +14,7 @@ jest.mock('child_process', () => ({
 
 process.env = { TASKSFILE_TEST: 'tasksfile test' }
 
-describe('api', () => {
+describe('sh()', () => {
   let logger: any
   let spawnProcessMock: any
 
@@ -25,142 +25,132 @@ describe('api', () => {
       title: jest.fn(),
       warning: jest.fn()
     }
+    execSyncMock.mockReset()
+    spawnMock.mockReset()
+    execSyncMock.mockReturnValue('output')
+    spawnProcessMock = new EventEmitter()
+    spawnProcessMock.stdout = new EventEmitter()
+    spawnMock.mockReturnValue(spawnProcessMock)
   })
 
-  describe('sh()', () => {
-    beforeEach(() => {
-      execSyncMock.mockReset()
-      spawnMock.mockReset()
-      execSyncMock.mockReturnValue('output')
-      spawnProcessMock = new EventEmitter()
-      spawnProcessMock.stdout = new EventEmitter()
-      spawnMock.mockReturnValue(spawnProcessMock)
+  describe('sync version', () => {
+    describe('with stdio=pipe', () => {
+      it('should execute basic shell commands', () => {
+        const output = api.sh('echo "echo test"', { stdio: 'pipe' }, logger)
+        expect(execSyncMock.mock.calls[0][0]).toEqual('echo "echo test"')
+        expect(execSyncMock.mock.calls[0][1]).toHaveProperty('stdio', 'pipe')
+        expect(output).toEqual('output')
+        expect(logger.log).toHaveBeenCalledWith(chalk.bold('echo "echo test"'))
+      })
+
+      it('should throw an error if command fails', () => {
+        execSyncMock.mockImplementation(() => {
+          throw new Error('test')
+        })
+        expect(() => {
+          api.sh('node ./ghost.js', { stdio: 'pipe' }, logger)
+        }).toThrow(ShellError)
+      })
+
+      it('should have access to environment variables by default', () => {
+        api.sh('cli-command', { stdio: 'pipe' }, logger)
+        expect(execSyncMock.mock.calls[0][1].env).toHaveProperty(
+          'TASKSFILE_TEST',
+          'tasksfile test'
+        )
+      })
+
+      it('should include in PATH node_modules/.bin', () => {
+        api.sh('cli-command', { stdio: 'pipe' }, logger)
+        expect(execSyncMock.mock.calls[0][1].env.PATH).toContain(
+          'node_modules/.bin'
+        )
+      })
     })
 
-    describe('sync version', () => {
-      describe('with stdio=pipe', () => {
-        it('should execute basic shell commands', () => {
-          const output = api.sh('echo "echo test"', { stdio: 'pipe' }, logger)
-          expect(execSyncMock.mock.calls[0][0]).toEqual('echo "echo test"')
-          expect(execSyncMock.mock.calls[0][1]).toHaveProperty('stdio', 'pipe')
-          expect(output).toEqual('output')
-          expect(logger.log).toHaveBeenCalledWith(
-            chalk.bold('echo "echo test"')
-          )
-        })
+    describe('with stdio=inherit', () => {
+      it('should execute basic shell commands', () => {
+        api.sh('cli-command', {}, logger)
+        expect(execSyncMock.mock.calls[0][0]).toEqual('cli-command')
+        expect(execSyncMock.mock.calls[0][1]).toHaveProperty('stdio', 'inherit')
+        expect(logger.log).toHaveBeenCalledWith(chalk.bold('cli-command'))
+      })
+    })
+  })
 
-        it('should throw an error if command fails', () => {
-          execSyncMock.mockImplementation(() => {
-            throw new Error('test')
+  describe('async version', () => {
+    describe('with stdio=pipe', () => {
+      it('should execute basic shell commands', () => {
+        const runProcess = api
+          .sh('cli-cmd', { async: true, stdio: 'pipe' }, logger)
+          .then(output => {
+            expect(spawnMock.mock.calls[0][0]).toEqual('cli-cmd')
+            expect(spawnMock.mock.calls[0][1]).toHaveProperty('stdio', 'pipe')
+            expect(output).toEqual('output')
+            expect(logger.log).toHaveBeenCalledWith(chalk.bold('cli-cmd'))
           })
-          expect(() => {
-            api.sh('node ./ghost.js', { stdio: 'pipe' }, logger)
-          }).toThrow(ShellError)
-        })
-
-        it('should have access to environment variables by default', () => {
-          api.sh('cli-command', { stdio: 'pipe' }, logger)
-          expect(execSyncMock.mock.calls[0][1].env).toHaveProperty(
-            'TASKSFILE_TEST',
-            'tasksfile test'
-          )
-        })
-
-        it('should include in PATH node_modules/.bin', () => {
-          api.sh('cli-command', { stdio: 'pipe' }, logger)
-          expect(execSyncMock.mock.calls[0][1].env.PATH).toContain(
-            'node_modules/.bin'
-          )
-        })
+        spawnProcessMock.stdout.emit('data', 'output')
+        spawnProcessMock.emit('close', 0)
+        return runProcess
       })
 
-      describe('with stdio=inherit', () => {
-        it('should execute basic shell commands', () => {
-          api.sh('cli-command', {}, logger)
-          expect(execSyncMock.mock.calls[0][0]).toEqual('cli-command')
-          expect(execSyncMock.mock.calls[0][1]).toHaveProperty(
-            'stdio',
-            'inherit'
-          )
-          expect(logger.log).toHaveBeenCalledWith(chalk.bold('cli-command'))
-        })
+      it('should have access to environment variables by default', () => {
+        const runProcess = api
+          .sh('cli-cmd', { async: true, stdio: 'pipe' }, logger)
+          .then(() => {
+            expect(spawnMock.mock.calls[0][1].env).toHaveProperty(
+              'TASKSFILE_TEST',
+              'tasksfile test'
+            )
+          })
+        spawnProcessMock.stdout.emit('data', 'output')
+        spawnProcessMock.emit('close', 0)
+        return runProcess
+      })
+
+      it('should reject with an error if command fails', () => {
+        const runProcess = api.sh(
+          'cli-cmd',
+          { async: true, stdio: 'pipe' },
+          logger
+        )
+        spawnProcessMock.emit('close', 1)
+        return expect(runProcess).rejects.toHaveProperty(
+          'message',
+          'Command failed: cli-cmd with exit code 1'
+        )
+      })
+
+      it('should include in PATH node_modules/.bin', () => {
+        const runProcess = api
+          .sh('cli-cmd', { async: true, stdio: 'pipe' }, logger)
+          .then(() => {
+            expect(spawnMock.mock.calls[0][1].env.PATH).toContain(
+              'node_modules/.bin'
+            )
+          })
+        spawnProcessMock.stdout.emit('data', 'output')
+        spawnProcessMock.emit('close', 0)
+        return runProcess
       })
     })
 
-    describe('async version', () => {
-      describe('with stdio=pipe', () => {
-        it('should execute basic shell commands', () => {
-          const runProcess = api
-            .sh('cli-cmd', { async: true, stdio: 'pipe' }, logger)
-            .then(output => {
-              expect(spawnMock.mock.calls[0][0]).toEqual('cli-cmd')
-              expect(spawnMock.mock.calls[0][1]).toHaveProperty('stdio', 'pipe')
-              expect(output).toEqual('output')
-              expect(logger.log).toHaveBeenCalledWith(chalk.bold('cli-cmd'))
-            })
-          spawnProcessMock.stdout.emit('data', 'output')
-          spawnProcessMock.emit('close', 0)
-          return runProcess
-        })
-
-        it('should have access to environment variables by default', () => {
-          const runProcess = api
-            .sh('cli-cmd', { async: true, stdio: 'pipe' }, logger)
-            .then(() => {
-              expect(spawnMock.mock.calls[0][1].env).toHaveProperty(
-                'TASKSFILE_TEST',
-                'tasksfile test'
-              )
-            })
-          spawnProcessMock.stdout.emit('data', 'output')
-          spawnProcessMock.emit('close', 0)
-          return runProcess
-        })
-
-        it('should reject with an error if command fails', () => {
-          const runProcess = api.sh(
-            'cli-cmd',
-            { async: true, stdio: 'pipe' },
-            logger
-          )
-          spawnProcessMock.emit('close', 1)
-          return expect(runProcess).rejects.toHaveProperty(
-            'message',
-            'Command failed: cli-cmd with exit code 1'
-          )
-        })
-
-        it('should include in PATH node_modules/.bin', () => {
-          const runProcess = api
-            .sh('cli-cmd', { async: true, stdio: 'pipe' }, logger)
-            .then(() => {
-              expect(spawnMock.mock.calls[0][1].env.PATH).toContain(
-                'node_modules/.bin'
-              )
-            })
-          spawnProcessMock.stdout.emit('data', 'output')
-          spawnProcessMock.emit('close', 0)
-          return runProcess
-        })
-      })
-
-      describe('with stdio=inherit', () => {
-        it('should execute basic shell commands', () => {
-          const runProcess = api
-            .sh('cli-cmd', { async: true, stdio: 'inherit' }, logger)
-            .then(output => {
-              expect(spawnMock.mock.calls[0][0]).toEqual('cli-cmd')
-              expect(spawnMock.mock.calls[0][1]).toHaveProperty(
-                'stdio',
-                'inherit'
-              )
-              expect(output).toEqual(null)
-              expect(logger.log).toHaveBeenCalledWith(chalk.bold('cli-cmd'))
-            })
-          spawnProcessMock.stdout.emit('data', 'output')
-          spawnProcessMock.emit('close', 0)
-          return runProcess
-        })
+    describe('with stdio=inherit', () => {
+      it('should execute basic shell commands', () => {
+        const runProcess = api
+          .sh('cli-cmd', { async: true, stdio: 'inherit' }, logger)
+          .then(output => {
+            expect(spawnMock.mock.calls[0][0]).toEqual('cli-cmd')
+            expect(spawnMock.mock.calls[0][1]).toHaveProperty(
+              'stdio',
+              'inherit'
+            )
+            expect(output).toEqual(null)
+            expect(logger.log).toHaveBeenCalledWith(chalk.bold('cli-cmd'))
+          })
+        spawnProcessMock.stdout.emit('data', 'output')
+        spawnProcessMock.emit('close', 0)
+        return runProcess
       })
     })
   })
