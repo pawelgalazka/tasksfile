0fd07a116df23672936161274996d10b7a0ab168
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-19 15:08:08 +1000

    E2E tests: rename sh function

--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -1,27 +1,26 @@
 /* eslint-env jest */
-import { execSync } from 'child_process'
+import { execSync as orgExecSync } from 'child_process'
 
 describe('tasksfile', () => {
   const scriptPath = '../../bin/task.js'
-  function sh(cmd: string) {
-    return execSync(cmd, {
+  function execSync(cmd: string) {
+    return orgExecSync(cmd, {
       cwd: './test/sandbox',
       env: {
         ...process.env,
         FORCE_COLOR: '0'
-      },
-      stdio: 'pipe'
+      }
     }).toString()
   }
 
   it('executes simple task', () => {
-    expect(sh(`${scriptPath} echo 1 2 3 --foo --bar`)).toContain(
+    expect(execSync(`${scriptPath} echo 1 2 3 --foo --bar`)).toContain(
       "echo [ { foo: true, bar: true }, '1', '2', '3' ]\n"
     )
   })
 
   it('executes shell commands in a task', () => {
-    const output = sh(`${scriptPath} commands`)
+    const output = execSync(`${scriptPath} commands`)
     expect(output).toContain(
       'echo "sync terminal"\nsync terminal\necho "sync pipe"\noutput sync pipe'
     )
@@ -30,28 +29,28 @@ describe('tasksfile', () => {
   })
 
   it('executes name spaced tasks', () => {
-    expect(sh(`${scriptPath} nested:echo 1 2 3 --foo --bar`)).toContain(
+    expect(execSync(`${scriptPath} nested:echo 1 2 3 --foo --bar`)).toContain(
       "echo [ { foo: true, bar: true }, '1', '2', '3' ]\n"
     )
   })
 
   it('includes ./node_modules/.bin to PATH when executing commands', () => {
-    sh('cp -p ./scripts/hello.js ./node_modules/.bin/hello')
-    expect(sh(`${scriptPath} localbin`)).toContain('Hello!')
+    execSync('cp -p ./scripts/hello.js ./node_modules/.bin/hello')
+    expect(execSync(`${scriptPath} localbin`)).toContain('Hello!')
   })
 
   it('executes tasks with async and await', () => {
-    expect(sh(`${scriptPath} asyncawait`)).toContain(
+    expect(execSync(`${scriptPath} asyncawait`)).toContain(
       'echo "async and await"\noutput async and await\n\nafter await\n'
     )
   })
 
   it('displays help for a task', () => {
-    expect(sh(`${scriptPath} echo --help`)).toContain('Simple echo task')
+    expect(execSync(`${scriptPath} echo --help`)).toContain('Simple echo task')
   })
 
   it('displays error from executed command', () => {
-    expect(() => sh(`${scriptPath} error`)).toThrow(
+    expect(() => execSync(`${scriptPath} error`)).toThrow(
       `Command failed: ${scriptPath} error`
     )
   })
