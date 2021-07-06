c579c325fdbaa8497effbe00f95b4b4ff65a234e
Brian3647 <victoris3647@gmail.com>
2021-07-06 18:25:11 +0200

    Add handling of missing script

--- bin/task.js
+++ bin/task.js
@@ -5,8 +5,17 @@ const packageJson = require(path.resolve('./package.json'))
 
 const taskScript = packageJson.scripts.task
 
+if (taskScript === undefined) {
+  // prettier-ignore
+  console.error('Mising \'task\' script in package.json')
+  process.exit(1)
+}
+
 try {
-  execSync(`${taskScript} ${process.argv.slice(2).join(' ')}`, {shell: true, stdio: 'inherit'})
+  execSync(`${taskScript} ${process.argv.slice(2).join(' ')}`, {
+    shell: true,
+    stdio: 'inherit',
+  })
 } catch (error) {
   process.exit(1)
 }
